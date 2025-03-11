032e5c4f028919fe6c1489b493aaf31c23199330
Mickey Li <mickeyhli@outlook.com>
2025-03-11 15:20:43 +0000

    Added pre build script

---
+++ pre-build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+readonly VERSION_ROS1="ROS1"
+readonly VERSION_ROS2="ROS2"
+readonly VERSION_HUMBLE="humble"
+
+pushd `pwd` > /dev/null
+cd `dirname $0`
+echo "Working Path: "`pwd`
+
+ROS_VERSION=""
+ROS_HUMBLE=""
+
+# Set working ROS version
+if [ "$1" = "ROS2" ]; then
+    ROS_VERSION=${VERSION_ROS2}
+elif [ "$1" = "humble" ]; then
+    ROS_VERSION=${VERSION_ROS2}
+    ROS_HUMBLE=${VERSION_HUMBLE}
+elif [ "$1" = "ROS1" ]; then
+    ROS_VERSION=${VERSION_ROS1}
+else
+    echo "Invalid Argument"
+    exit
+fi
+echo "ROS version is: "$ROS_VERSION
+
+# clear `build/` folder.
+# TODO: Do not clear these folders, if the last build is based on the same ROS version.
+rm -rf ../../build/
+rm -rf ../../devel/
+rm -rf ../../install/
+# clear src/CMakeLists.txt if it exists.
+if [ -f ../CMakeLists.txt ]; then
+    rm -f ../CMakeLists.txt
+fi
+
+# exit
+
+# substitute the files/folders: CMakeList.txt, package.xml(s)
+if [ ${ROS_VERSION} = ${VERSION_ROS1} ]; then
+    if [ -f package.xml ]; then
+        rm package.xml
+    fi
+    cp -f package_ROS1.xml package.xml
+elif [ ${ROS_VERSION} = ${VERSION_ROS2} ]; then
+    if [ -f package.xml ]; then
+        rm package.xml
+    fi
+    cp -f package_ROS2.xml package.xml
+    cp -rf launch_ROS2/ launch/
+fi
+
